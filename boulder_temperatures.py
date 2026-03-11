2ccb04fbf4bedc3970ef4d82399ea964d2d88bc6
Demi <deda2367@colorado.edu>
2026-03-10 21:33:44 -0600

    Add my name

--- boulder_temperatures.py
+++ boulder_temperatures.py
@@ -10,7 +10,7 @@ Instructions:
 """
 
 # TODO: Add your name here
-STUDENT_NAME = ""  # Example: "Alice Johnson"
+STUDENT_NAME = "Demi Davoll"  # Example: "Alice Johnson"
 
 
 def load_temperatures(filepath):
